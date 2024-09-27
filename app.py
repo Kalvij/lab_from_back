167ab3c0a6569e96dba60db60b99dfbfeb195157
Kalvij <yefimkina04@mail.ru>
2024-09-27 17:03:22 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на Flask"
+    return """
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ярославцев Богдан Всеволодович. Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ ФБ, Лабораторная 1
+        </header>
+
+        <main>
+            
+        </main>
+
+
+        <footer>
+            <hr>
+            &copy;Ярославцев Богдан, ФБИ-24, 3 курс, 2024   
+        </footer>
+    </body>
+</html>
+"""
