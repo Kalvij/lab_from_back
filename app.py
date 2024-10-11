7a44a762df9c8b6d3d3dd091a7f9ca492d59b825
Kalvij <yefimkina04@mail.ru>
2024-10-11 15:34:51 +0700

    Имя студента

--- app.py
+++ app.py
@@ -216,4 +216,5 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Ярославцев Богдан'
+    return render_template('example.html', name=name)
