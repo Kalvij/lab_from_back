d4d82f45647353152841b208256ed2ec2ebe1f6c
Kalvij <yefimkina04@mail.ru>
2024-10-25 17:21:05 +0700

    Рефакторинг: выделение первой лабораторной

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
-from lab2 import lab2
-from lab3 import lab3
+
 app = Flask(__name__)
 app.register_blueprint(lab1)
-app.register_blueprint(lab2)
-app.register_blueprint(lab3)
 
 @app.route("/")
 @app.route("/index")
@@ -55,8 +52,6 @@ def menu():
                 </ol>
             </div>
         </main>
-
-
         <footer>
             <hr>
             &copy;Ярославцев Богдан, ФБИ-24, 3 курс, 2024   
@@ -65,23 +60,133 @@ def menu():
 </html>
 """
 
-@app.errorhandler(404)
-def not_found(err):
-    return '''
+@app.route('/lab2/a')
+def a():
+    return 'без слэша'
+
+@app.route('/lab2/a/')
+def a2():
+    return 'со слэшем'
+
+flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id>= len(flower_list):
+        return "Такого цветка нет", 404
+    else:
+        return "Цветок: " + flower_list[flower_id]
+
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    if not name:
+        return "Вы не задали имя цветка", 400
+    flower_list.append(name)
+    return f'''
 <!doctype html>
 <html>
-    <head>
-        <title>НГТУ, ФБ, Лабораторные работы</title>
-        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
-    </head>
     <body>
-        <header>
-            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
-        </header>
-         <h2>Ошибка 404 - такой страницы не существует</h2>
-        <footer>
-            &copy; Ярославцев Богдан, ФБИ-24, 3 курс, 2024
-        </footer>
+    <h1>Добавлен новый цветок</h1>
+    <p>Название нового цветка: {name}</p>
+    <p>Всего цветов: {len(flower_list)}</p>
+    <p>Полный список: {flower_list}</p>
     </body>
 </html>
 '''
+@app.route('/lab2/flowers/')
+def show_all_flowers():
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1> Все цветы </h1>
+    <p> Всего цветов: {len(flower_list)} </p>
+    <ul>
+        {"".join(f"<li>{flower}</li>" for flower in flower_list)}
+    </ul>
+    </body>
+</html>
+'''
+
+
+@app.route('/lab2/example')
+def example():
+    name = 'Ярославцев Богдан'
+    numlab = '2'
+    group = 'ФБИ-24'
+    numbCourse = '3 курс'
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html', name=name, numlab=numlab, group=group, numbCourse=numbCourse, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
+
+@app.route('/lab2/filters')
+def filters():
+    phrase = 'О <b>сколько</b> <u>нам</u> <i>открытий чудных...'
+    return render_template('filter.html', phrase=phrase)
+
+@app.route('/lab2/calc/<int:a>/<int:b>')
+def calc(a, b):
+    sum = a + b
+    minus = a - b
+    mul = a * b
+    if b == 0:
+        delenie = "Делить на ноль нельзя"
+    else:
+        delenie = a / b
+    #div_result = "Деление на ноль невозможно" if b == 0 else num1 / num2
+    
+    stepen = a ** b
+    return f'''
+<!doctype html>
+<html>
+    <body style="background: #eb99fc5b;">
+    <h1> Расчет с параметрами </h1>
+    <p style="color: black;text-shadow: 1px 1px 0px white, -1px -1px 0px white, 1px -1px 0px white, -1px 1px 0px white;font-size: 23px;text-decoration: none;margin: 10px">{a} + {b} = {sum}</p>
+    <p style="color: black;text-shadow: 1px 1px 0px white, -1px -1px 0px white, 1px -1px 0px white, -1px 1px 0px white;font-size: 23px;text-decoration: none;margin: 10px">{a} - {b} = {minus}</p>
+    <p style="color: black;text-shadow: 1px 1px 0px white, -1px -1px 0px white, 1px -1px 0px white, -1px 1px 0px white;font-size: 23px;text-decoration: none;margin: 10px">{a} * {b} = {mul}</p>
+    <p style="color: black;text-shadow: 1px 1px 0px white, -1px -1px 0px white, 1px -1px 0px white, -1px 1px 0px white;font-size: 23px;text-decoration: none;margin: 10px">{a} / {b} = {delenie}</p>
+    <p style="color: black;text-shadow: 1px 1px 0px white, -1px -1px 0px white, 1px -1px 0px white, -1px 1px 0px white;font-size: 23px;text-decoration: none;margin: 10px">{a} <sup> {b} </sup>= {stepen}</p>
+    </body>
+</html>
+'''
+@app.route('/lab2/calc/')
+def redirect_to_default():
+    return redirect('/lab2/calc/1/1')
+@app.route('/lab2/calc/<int:a>')
+def redirect_with_default_b(a):
+    return redirect(f'/lab2/calc/{a}/1')
+
+book_list = [
+    {'author': 'Агата Кристи', 'name': 'Десять негритят', 'ganre': 'роман', 'pages': 1056},
+    {'author': 'Фрэнсис Скотт Фицджеральд', 'name': 'Великий Гэтсби', 'ganre': 'роман', 'pages': 1152},
+    {'author': 'Джордж Оруэлл', 'name': '1984', 'ganre': 'роман-антиутопия', 'pages': 1360},
+    {'author': 'Джейн Остин', 'name': 'Гордость и предубеждение', 'ganre': 'роман', 'pages': 1376},
+    {'author': 'Эрих Мария Ремарк', 'name': 'Три товарища', 'ganre': 'роман', 'pages': 1424},
+    {'author': 'Габриэль Гарсиа Маркес', 'name': 'Сто лет одиночества', 'ganre': 'роман', 'pages': 1472},
+    {'author': 'Федор Достоевский', 'name': 'Преступление и наказание', 'ganre': 'роман', 'pages': 1492},
+    {'author': 'Виктор Гюго', 'name': 'Отверженные', 'ganre': 'роман', 'pages': 3031},
+    {'author': 'Антон Чехов', 'name': 'Рассказы', 'ganre': 'автобиография', 'pages': 3600}
+]
+@app.route('/lab2/books')
+def books():
+    return render_template('books.html', book_list=book_list)
+
+@app.route('/lab2/car')
+def car():
+    car_list = [
+    {'title': 'Toyota Camry', 'description': 'Комфортный седан с отличной репутацией надежности.', 'price': 25000, 'image_url': url_for('static', filename='toy.jpg')},
+    {'title': 'Honda Civic', 'description': 'Экономичный и динамичный автомобиль с современным дизайном.', 'price': 22000, 'image_url': url_for('static', filename='honda.jpg')},
+    {'title': 'Ford Mustang', 'description': 'Классический американский спорткар с мощным двигателем.', 'price': 35000, 'image_url': url_for('static', filename='ford.jpg')},
+    {'title': 'Tesla Model 3', 'description': 'Электрический седан с передовыми технологиями и высокой автономностью.', 'price': 40000, 'image_url': url_for('static', filename='tesla.webp')},
+    {'title': 'BMW X5', 'description': 'Премиальный внедорожник с роскошным интерьером и превосходными характеристиками.', 'price': 60000, 'image_url': url_for('static', filename='bmw.webp')},
+]
+    return render_template('car.html', car_list=car_list)
