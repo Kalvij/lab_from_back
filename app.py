245e5ba86c3bc6c7b07d254aa9f1532e4487be52
Kalvij <yefimkina04@mail.ru>
2024-12-25 02:39:28 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -8,11 +8,30 @@ from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from urllib.parse import quote
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'bogdan_yroslavtsev_orm'
+    db_user = 'bogdan_yroslavtsev_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "bogdan_yroslavtsev_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bogdan_yroslavtsev_orm.db'
+    
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
