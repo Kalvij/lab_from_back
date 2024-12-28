fff7ff127a222d305bc06c839c7f65322febe58e
Kalvij <yefimkina04@mail.ru>
2024-12-28 16:42:43 +0700

    Заготовка к 9й лабораторной

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 import os
 from os import path
 from flask_sqlalchemy import SQLAlchemy
@@ -52,6 +53,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 
 @app.route("/")
